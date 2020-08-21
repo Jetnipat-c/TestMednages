28ef47911b74abb9942a486785660b4e7f80ea0f
alonecheer <alonecheer@hotmail.com>
2020-08-22 01:27:47 +0700

    design

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,28 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
-export default function Home() {
+import React from "react";
+import { Layout, Menu } from "antd";
+const { Content } = Layout;
+import SiderComponent from "../src/components/sider/sider.js";
+import HeaderComponent from "../src/components/header/header.js";
+import FooterComponent from "../src/components/footer/footer.js";
+ import HomeStyleWrapper from "./home.style";
+const home = () => {
   return (
-    <div className={styles.container}>
-      test deploy
-    </div>
-  )
-}
+    <HomeStyleWrapper>
+      <title>Homepage</title>
+      <Layout>
+        <SiderComponent />
+        <Layout>
+          <HeaderComponent />
+          <Content style={{ margin: "24px 16px 0" }}>
+            <div
+              className="site-layout-background"
+              style={{ padding: 24, minHeight: "100vh" }}
+            ></div>
+          </Content>
+          <FooterComponent />
+        </Layout>
+      </Layout>
+    </HomeStyleWrapper>
+  );
+};
+export default home;
