bc5127650730d0a59f4ba7081cadd937ac8f4038
alonecheer <alonecheer@hotmail.com>
2020-08-22 03:09:04 +0700

    delete vs

--- pages/visualization/visualization.js
+++
@@ -1,9 +0,0 @@
-import VsStyleWrapper from "./visualization.style.js";
-const VSComponent = () => {
-    return(
-        <VsStyleWrapper>
-        vS
-        </VsStyleWrapper>
-    )
-}
-export default VSComponent;
