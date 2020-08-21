14ae27ea0c8fbb6a9413b915a93b14533e5ecea3
alonecheer <alonecheer@hotmail.com>
2020-08-22 01:46:03 +0700

    dd

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Layout, Menu } from "antd";
 const { Content } = Layout;
-import SiderComponent from "../src/components/sider/sider.js";
-//import HeaderComponent from "../src/components/header/header.js";
-import FooterComponent from "../src/components/footer/footer.js";
+import SiderComponent from "../src/components/sider/sider";
+import HeaderComponent from "../src/components/header/header";
+import FooterComponent from "../src/components/footer/footer";
  import HomeStyleWrapper from "./home.style";
 const home = () => {
   return (
@@ -12,7 +12,7 @@ const home = () => {
       <Layout>
         <SiderComponent />
         <Layout>
-          {/* <HeaderComponent /> */}
+          <HeaderComponent />
           <Content style={{ margin: "24px 16px 0" }}>
             <div
               className="site-layout-background"
